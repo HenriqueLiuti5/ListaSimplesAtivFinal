87f371f9abdc6e7470cc6414cb9c1f606bc4aa48
HenriqueLiuti5 <henrique.liuti@edu.unifil.br>
2025-04-10 20:44:47 -0300

    Estrutura da atividade

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        ListaSimples listaSimples = new ListaSimples();
         Scanner sc = new Scanner(System.in);
 
         int opcao;
