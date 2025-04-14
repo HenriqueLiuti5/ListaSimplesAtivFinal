17df2c6532ffe8ccd2f3e055827f5105dde8ffe5
Henrique <henrique.liuti@edu.unifil.br>
2025-04-14 01:15:44 -0300

    COMMIT FINAL

--- src/ListaSimples.java
+++ src/ListaSimples.java
@@ -1,101 +1,195 @@
+import java.util.Arrays;
+
 public class ListaSimples implements IEstruturaSimples {
+    private Object[] elementos;
+    private int tamanho;
+
+    public ListaSimples(int capacidadeInicial) {
+        this.elementos = new Object[capacidadeInicial];
+        this.tamanho = 0;
+    }
+
     @Override
     public void inserirElemento(Object elemento) {
-
+        if (estaCheia()) {
+            dobrarCapacidade();
+        }
+        elementos[tamanho++] = elemento;
     }
 
     @Override
     public void inserirElementoIndice(Object elemento, int indice) {
-
+        if (indice < 0 || indice > tamanho) {
+            System.out.println("Índice inválido!");
+            return;
+        }
+        if (estaCheia()) {
+            dobrarCapacidade();
+        }
+        for (int i = tamanho; i > indice; i--) {
+            elementos[i] = elementos[i - 1];
+        }
+        elementos[indice] = elemento;
+        tamanho++;
     }
 
     @Override
     public void inserirSequencia(Object elementos) {
-
+        Object[] sequencia = (Object[]) elementos;
+        for (Object elemento : sequencia) {
+            inserirElemento(elemento);
+        }
     }
 
     @Override
     public boolean removerElemento() {
-        return false;
+        if (estaVazia()) return false;
+        for (int i = 0; i < tamanho - 1; i++) {
+            elementos[i] = elementos[i + 1];
+        }
+        elementos[--tamanho] = null;
+        return true;
     }
 
     @Override
     public Object removerIndice(int indice) {
-        return null;
+        if (indice < 0 || indice >= tamanho) {
+            System.out.println("Índice inválido!");
+            return null;
+        }
+        Object removido = elementos[indice];
+        for (int i = indice; i < tamanho - 1; i++) {
+            elementos[i] = elementos[i + 1];
+        }
+        elementos[--tamanho] = null;
+        return removido;
     }
 
     @Override
     public void removerSequencia(Object elementos) {
-
+        Object[] sequencia = (Object[]) elementos;
+        for (Object elem : sequencia) {
+            removerTodasOcorrencias(elem);
+        }
     }
 
     @Override
     public void removerTodasOcorrencias(Object elemento) {
-
+        int i = 0;
+        while (i < tamanho) {
+            if (elementos[i].equals(elemento)) {
+                removerIndice(i);
+            } else {
+                i++;
+            }
+        }
     }
 
     @Override
     public boolean estaCheia() {
-        return false;
+        return tamanho == elementos.length;
     }
 
     @Override
     public boolean estaVazia() {
-        return false;
+        return tamanho == 0;
     }
 
     @Override
     public boolean buscarElemento(Object elemento) {
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i].equals(elemento)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Object buscarElementoIndice(int indice) {
-        return null;
+        if (indice < 0 || indice >= tamanho) {
+            System.out.println("Índice inválido!");
+            return null;
+        }
+        return elementos[indice];
     }
 
     @Override
     public void ordenarCrescente() {
-
+        for (int i = 0; i < tamanho - 1; i++) {
+            for (int j = 0; j < tamanho - i - 1; j++) {
+                Comparable atual = (Comparable) elementos[j];
+                Comparable proximo = (Comparable) elementos[j + 1];
+                if (atual.compareTo(proximo) > 0) {
+                    Object temp = elementos[j];
+                    elementos[j] = elementos[j + 1];
+                    elementos[j + 1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public void ordenarDecrescente() {
-
+        for (int i = 0; i < tamanho - 1; i++) {
+            for (int j = 0; j < tamanho - i - 1; j++) {
+                Comparable atual = (Comparable) elementos[j];
+                Comparable proximo = (Comparable) elementos[j + 1];
+                if (atual.compareTo(proximo) < 0) {
+                    Object temp = elementos[j];
+                    elementos[j] = elementos[j + 1];
+                    elementos[j + 1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public int quantidadeElementos() {
-        return 0;
+        return tamanho;
     }
 
     @Override
     public void dobrarCapacidade() {
-
+        elementos = Arrays.copyOf(elementos, elementos.length * 2);
     }
 
     @Override
     public void editarElemento(Object elementoAntigo, Object elementoNovo) {
-
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i].equals(elementoAntigo)) {
+                elementos[i] = elementoNovo;
+            }
+        }
     }
 
     @Override
     public void limpar() {
-
+        for (int i = 0; i < tamanho; i++) {
+            elementos[i] = null;
+        }
+        tamanho = 0;
     }
 
     @Override
     public void exibir() {
-
+        System.out.print("Lista: [");
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(elementos[i]);
+            if (i < tamanho - 1) System.out.print(", ");
+        }
+        System.out.println("]");
     }
 
     @Override
     public Object obterPrimeiroElemento() {
-        return null;
+        if (estaVazia()) return null;
+        return elementos[0];
     }
 
     @Override
     public Object obterUltimoElemento() {
-        return null;
+        if (estaVazia()) return null;
+        return elementos[tamanho - 1];
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        ListaSimples listaSimples = new ListaSimples();
+        ListaSimples listaSimples = new ListaSimples(5);
         Scanner sc = new Scanner(System.in);
 
         int opcao;
 
         do {
-            System.out.println("Menu");
+            System.out.println("\nMenu");
             System.out.println("1. Inserir elemento");
             System.out.println("2. Inserir elemento em índice");
             System.out.println("3. Inserir sequência");
@@ -23,42 +23,126 @@ public class Main {
             System.out.println("11. Quantidade de elementos");
             System.out.println("12. Limpar lista");
             System.out.println("13. Exibir lista");
+            System.out.println("14. Editar elemento");
+            System.out.println("15. Obter primeiro elemento");
+            System.out.println("16. Obter último elemento");
+            System.out.println("17. Dobrar capacidade da lista");
             System.out.println("0. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = sc.nextInt();
+            sc.nextLine();
 
             switch (opcao) {
                 case 1:
+                    System.out.print("Elemento: ");
+                    listaSimples.inserirElemento(sc.nextLine());
+                    break;
 
                 case 2:
+                    System.out.print("Elemento: ");
+                    Object elemento = sc.nextLine();
+                    System.out.print("Índice: ");
+                    int indice = sc.nextInt();
+                    sc.nextLine();
+                    listaSimples.inserirElementoIndice(elemento, indice);
+                    break;
 
                 case 3:
+                    System.out.print("Quantidade de elementos na sequência: ");
+                    int qtd = sc.nextInt();
+                    sc.nextLine();
+                    Object[] sequencia = new Object[qtd];
+                    for (int i = 0; i < qtd; i++) {
+                        System.out.print("Elemento " + (i + 1) + ": ");
+                        sequencia[i] = sc.nextLine();
+                    }
+                    listaSimples.inserirSequencia(sequencia);
+                    break;
 
                 case 4:
+                    listaSimples.removerElemento();
+                    break;
 
                 case 5:
+                    System.out.print("Índice para remover: ");
+                    int idx = sc.nextInt();
+                    sc.nextLine();
+                    listaSimples.removerIndice(idx);
+                    break;
 
                 case 6:
+                    System.out.print("Elemento a remover todas ocorrências: ");
+                    Object remov = sc.nextLine();
+                    listaSimples.removerTodasOcorrencias(remov);
+                    break;
 
                 case 7:
+                    System.out.print("Elemento para buscar: ");
+                    Object buscado = sc.nextLine();
+                    boolean encontrado = listaSimples.buscarElemento(buscado);
+                    System.out.println("Encontrado: " + encontrado);
+                    break;
 
                 case 8:
+                    System.out.print("Índice para buscar: ");
+                    int idxBusca = sc.nextInt();
+                    sc.nextLine();
+                    Object retorno = listaSimples.buscarElementoIndice(idxBusca);
+                    System.out.println("Elemento encontrado: " + retorno);
+                    break;
 
                 case 9:
+                    listaSimples.ordenarCrescente();
+                    System.out.println("Lista ordenada em ordem crescente.");
+                    break;
 
                 case 10:
+                    listaSimples.ordenarDecrescente();
+                    System.out.println("Lista ordenada em ordem decrescente.");
+                    break;
 
                 case 11:
+                    System.out.println("Quantidade de elementos: " + listaSimples.quantidadeElementos());
+                    break;
 
                 case 12:
+                    listaSimples.limpar();
+                    System.out.println("Lista limpa.");
+                    break;
 
                 case 13:
+                    listaSimples.exibir();
+                    break;
+
+                case 14:
+                    System.out.print("Elemento antigo: ");
+                    Object antigo = sc.nextLine();
+                    System.out.print("Novo elemento: ");
+                    Object novo = sc.nextLine();
+                    listaSimples.editarElemento(antigo, novo);
+                    break;
+
+                case 15:
+                    System.out.println("Primeiro elemento: " + listaSimples.obterPrimeiroElemento());
+                    break;
+
+                case 16:
+                    System.out.println("Último elemento: " + listaSimples.obterUltimoElemento());
+                    break;
+
+                case 17:
+                    listaSimples.dobrarCapacidade();
+                    System.out.println("Capacidade da lista dobrada.");
+                    break;
 
                 case 0:
-                    System.out.println("Fim...");
+                    System.out.println("Finalizando...");
                     break;
+
+                default:
+                    System.out.println("Opção indisponível.");
             }
-        } while
-            (opcao != 0);
+
+        } while (opcao != 0);
     }
 }
